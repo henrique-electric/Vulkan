3b09d4b39ff5c6ab47adbdcffe2012abf0736f46
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-15 20:16:55 -0300

    test window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-#include <vulkan/vulkan.hpp>
-#include <iostream>
-#include <GLFW/glfw3.h>
+#include <window.hpp>
+
+void loop(void) {
+
+}
 
 int main(int argc, char const *argv[])
 {   
-    
+    win::Window window("Test", 1280, 720, loop);
+    window.run();
     return 0;
 }
