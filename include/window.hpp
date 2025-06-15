b775a9d2748a9bfebeb70cd9b5e87b0de68ee0a0
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-15 20:16:40 -0300

    Create base window class and functions

---
+++ include/window.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <functional>
+
+namespace win {
+    class Window
+    {
+    private:
+        GLFWwindow *m_glfwWin;
+        int m_height, m_width;
+        std::function<void(void)> m_mainLoop;
+
+        // functions
+        static void windowKeyHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+    public:
+
+        // Functions
+        Window(const char *name, int width, int height, std::function<void(void)> mainFunc);
+        ~Window();
+
+        void run();
+
+        inline int  shouldClose() { return glfwWindowShouldClose(m_glfwWin); }; // simple wrapper for GLFW windowShouldClose function
+        inline void handleEvents() { glfwPollEvents(); }; // Simple wrapper for GLFW event pooling function
+    };
+}
---
+++ src/window.cpp
@@ -0,0 +1,40 @@
+#include <window.hpp>
+
+namespace win {
+
+    // Window contructor
+    Window::Window(const char *name, int width, int height, std::function<void(void)> mainFunc) : m_width(width), m_height(height), m_mainLoop(mainFunc) {
+
+        // Initialize GLFW and set the window hints
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // Create the window and handle error
+        m_glfwWin = glfwCreateWindow(m_width, m_height, name, nullptr, nullptr);
+        if (m_glfwWin == NULL)
+            throw std::runtime_error("Error to create GLFW window"); // error creating the window
+        
+        glfwSetKeyCallback(m_glfwWin, windowKeyHandler); // Set the default handler for key inputs
+    }
+
+    // Object descontructor, frees the glfw resources
+    Window::~Window() {
+        glfwDestroyWindow(m_glfwWin);
+        glfwTerminate();
+    }
+
+    void Window::run() {
+        while(!shouldClose()) {
+            m_mainLoop();
+
+            handleEvents();
+        }
+    }
+} 
+
+namespace win {
+    void Window::windowKeyHandler(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        
+    }
+}
