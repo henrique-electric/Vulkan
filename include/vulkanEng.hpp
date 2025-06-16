690f00d1a14f683f561324923511b9611193f0b0
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-16 00:40:10 -0300

    Initialize the class members and move vulkan instance to protected

--- include/vulkanEng.hpp
+++ include/vulkanEng.hpp
@@ -1,20 +1,26 @@
 #pragma once
-#include <window.hpp>
 #include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
 #include <cstdint>
 #include <vector>
+#include <iostream>
 
 namespace vkEng
 {
     class VulkanEng
     {
+    protected:
+        VkInstance m_vkInstance{}; // Stores the vulkan instance itself
+
     private:
-        VkApplicationInfo m_appInfo; // store basic info about the vulkan application
-        VkInstanceCreateInfo m_instanceInfo; // Stores info used to create a vulkan instance
-        VkInstance m_vkInstance; // Stores the vulkan instance itself
-        
-        uint32_t m_extCount; // number of extensions
+        VkApplicationInfo m_appInfo{}; // store basic info about the vulkan application
+        VkInstanceCreateInfo m_instanceInfo{}; // Stores info used to create a vulkan instance
+
+        uint32_t m_extCount{}; // number of extensions
         std::vector<const char*> m_instExts; // Extension list
+
+        // Functions
+        static void listAvailableExtensions();
 
     public:
         VulkanEng(const char *appName, const char *engName);
