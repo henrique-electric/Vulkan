dd3b52e324489da182f3ca2848cb6b049426bf02
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-16 20:08:39 -0300

    Remove unnecessary object

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ void loop(void) {
 int main(int argc, char const *argv[])
 {   
     win::Window window("Test", 1280, 720, loop);
-    vkEng::VulkanEng eng("Test", "Engine");
     window.run();
     return 0;
 }
