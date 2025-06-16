d9c2aa0e264f64ccf9e4e14cc83e238e410013c6
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-15 21:25:21 -0300

    Test vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <window.hpp>
+#include <vulkanEng.hpp>
 
 void loop(void) {
 
 }
 
+
 int main(int argc, char const *argv[])
 {   
     win::Window window("Test", 1280, 720, loop);
+    vkEng::VulkanEng eng("Test", "Engine");
     window.run();
     return 0;
 }
