220468d69605b6f93120d4d0b60b109d8b9c0c32
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-15 21:25:35 -0300

    Create an header file to store the vulkan class

--- include/vulkanEng.hpp
+++ include/vulkanEng.hpp
@@ -1,8 +1,23 @@
 #pragma once
 #include <window.hpp>
 #include <vulkan/vulkan.hpp>
+#include <cstdint>
+#include <vector>
 
 namespace vkEng
 {
+    class VulkanEng
+    {
+    private:
+        VkApplicationInfo m_appInfo; // store basic info about the vulkan application
+        VkInstanceCreateInfo m_instanceInfo; // Stores info used to create a vulkan instance
+        VkInstance m_vkInstance; // Stores the vulkan instance itself
+        
+        uint32_t m_extCount; // number of extensions
+        std::vector<const char*> m_instExts; // Extension list
+
+    public:
+        VulkanEng(const char *appName, const char *engName);
+    };
     
 }
