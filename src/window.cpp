7aca50868d2622e4dab9cd3d572d714417d18018
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-16 00:42:37 -0300

    Destroy vulkan instance before destroy window

--- src/window.cpp
+++ src/window.cpp
@@ -1,9 +1,10 @@
+#include <utility>
 #include <window.hpp>
 
 namespace win {
 
     // Window contructor
-    Window::Window(const char *name, int width, int height, std::function<void(void)> mainFunc) : m_width(width), m_height(height), m_mainLoop(mainFunc) {
+    Window::Window(const char *name, int width, int height, std::function<void(void)> mainFunc) : VulkanEng(name, "Engine"), m_height(height), m_width(width), m_mainLoop(std::move(mainFunc)) {
 
         // Initialize GLFW and set the window hints
         glfwInit();
@@ -12,14 +13,15 @@ namespace win {
 
         // Create the window and handle error
         m_glfwWin = glfwCreateWindow(m_width, m_height, name, nullptr, nullptr);
-        if (m_glfwWin == NULL)
+        if (m_glfwWin == nullptr)
             throw std::runtime_error("Error to create GLFW window"); // error creating the window
-        
+
         glfwSetKeyCallback(m_glfwWin, windowKeyHandler); // Set the default handler for key inputs
     }
 
-    // Object descontructor, frees the glfw resources
+    // Object descontructor, frees the glfw resources and deletes vulkan instance
     Window::~Window() {
+        vkDestroyInstance(m_vkInstance, nullptr);
         glfwDestroyWindow(m_glfwWin);
         glfwTerminate();
     }
