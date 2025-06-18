f329b7901b7484476e020f97a13b3f9198b6ade7
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-06-18 19:16:45 -0300

    Create a file to store custom data types such as structs

---
+++ include/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <vulkan/vulkan.hpp>
+
+namespace vkEng {
+    struct gpuDevice {
+        VkPhysicalDevice device;
+        VkPhysicalDeviceProperties properties;
+        VkPhysicalDeviceFeatures features;
+    };
+}
