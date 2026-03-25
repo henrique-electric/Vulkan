7391ddcf19580ac4b1428bf426c7f83bd319f697
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-03-24 23:56:39 -0300

    Add comments to the debugger source file.

--- src/vulkanEng/vulkanEngDebug.cpp
+++ src/vulkanEng/vulkanEngDebug.cpp
@@ -2,6 +2,10 @@
 
 #ifdef DEBUG
 namespace vkEng {
+
+    /*
+		Clean up the resources used by the Vulkan debugger, because the debugger is an extension used by the instance, it is called at the end of the application.
+    */
     void VulkanEng::cleanDebugRes() {
         auto vkDestroyDebugUtilsMessengerEXT = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(m_vkInstance, "vkDestroyDebugUtilsMessengerEXT");
 
@@ -9,6 +13,9 @@ namespace vkEng {
         std::cout << "Destroyed the debugger mesenger\n";
     }
 
+    /*
+        Callback used by the Vulkan debugger, being called always a message gets into a validation layer.
+    */
     VkBool32 VulkanEng::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
                                       VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
                                       void *pUserData) {
@@ -19,6 +26,11 @@ namespace vkEng {
         return VK_FALSE;
     }
 
+    /*
+		Setup the validation layers and instance extensions required by the application, this function is called in the constructor of the engine, and it is used to 
+        load the validation layers and instance extensions required by the application, in this case, we load the VK_EXT_DEBUG_UTILS_EXTENSION_NAME extension to be 
+        able to use the Vulkan debugger, and we load the validation layers provided in the parameters of the function.
+    */
     void VulkanEng::setupDebugLayersAndExt(std::vector<const char*>& instanceLayers, std::vector<const char*>& gpuLayers) {
         m_instExts.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME); // load the extension for debbugin
 
@@ -29,6 +41,9 @@ namespace vkEng {
         m_instanceInfo.ppEnabledLayerNames = m_valLayers.data();
     }
 
+    /*
+		Intitialize the structure used to create the Debug Messenger, this structure is used to specify the details of the messages we want to receive from the validation layers.
+    */
     void VulkanEng::populateDebugMessengerStruct() {
         m_debugMessengerInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT; // We are using a structure to hold create info of the Debugger
 
@@ -44,15 +59,19 @@ namespace vkEng {
 
     }
     
+    /*
+		Handle the loading of the validation layers, this function is used to check if the validation layers provided in the parameters are available in the current 
+        system, and if they are, it loads them to be used by the instance.
+    */
     void VulkanEng::handleInstanceLayerLoading(const std::vector<const char*>& layersToLoad) {
-        uint32_t availableLayers = 0; // Variable to store all the available layers in the current system
-        vkEnumerateInstanceLayerProperties(&availableLayers, nullptr);  // Get How many layers are available in the current system
+        uint32_t availableLayers = 0; // Variable to store all the available layers in the current system.
+        vkEnumerateInstanceLayerProperties(&availableLayers, nullptr);  // Get How many layers are available in the current system.
         
-        VkLayerProperties currentLayersArr[availableLayers]; // Allocate a buffer to hold structures to each available layer
+        VkLayerProperties currentLayersArr[availableLayers]; // Allocate a buffer to hold structures to each available layer.
         
-        vkEnumerateInstanceLayerProperties(&availableLayers, currentLayersArr); // Load to buffer the layers available on the system
+        vkEnumerateInstanceLayerProperties(&availableLayers, currentLayersArr); // Load to buffer the layers available on the system.
         
-        // Check if the provided layers are available to load
+        // Check if the provided layers are available to load.
         for (int i = 0; i < layersToLoad.size() && i < availableLayers; i++) {
             if (utils::strcmp(currentLayersArr[i].layerName, layersToLoad[i]) == 0) {
                 printf("Unavailable layer in the current system. ----> %s\n", layersToLoad[i]);
@@ -64,6 +83,9 @@ namespace vkEng {
         
     }
 
+    /*
+        Create the debugger messenger system after seting up the structure, the callback and the layers to be used by the debugger.
+    */
     VkResult VulkanEng::setupDebugger() {
         auto createDebugMessenger = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(m_vkInstance, "vkCreateDebugUtilsMessengerEXT");
 
