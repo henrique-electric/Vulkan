33dbdd1d85acbe530ba2f8e94de928ad4f78ece3
jhpme <146463048+henrique-electric@users.noreply.github.com>
2026-07-02 16:48:24 -0300

    Add a handler for the swapchain.

--- include/types.hpp
+++ include/types.hpp
@@ -28,6 +28,7 @@ namespace vkEng {
         VkPhysicalDeviceFeatures features;
         std::vector<VkQueueFamilyProperties> queueProperties;
         VkDevice logicalInstance;
+        VkSwapchainKHR swapChain;
 
         uint8_t queueFamily; // In this project i'll be only using one queue, which one that has all the 3 main command processing capabilites 
         VkQueue queueInterface; // interface with the queue family being used
